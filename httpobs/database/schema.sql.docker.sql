39de1374a26d7bb785b94e864785e871db8d1f21
nikkkhilgoyal <goyalnikhil567@gmail.com>
2022-09-20 16:04:47 +0530

    for use scanner only.

--- httpobs/database/schema.sql.docker.sql
+++
@@ -1,4 +0,0 @@
-/* silly alphabetical naming requirements */
-
-ALTER ROLE httpobsapi LOGIN PASSWORD 'httpobsapipassword';
-ALTER ROLE httpobsscanner LOGIN PASSWORD 'httpobsscannerpassword';
--- httpobs/website/__init__.py
+++
@@ -1,4 +0,0 @@
-from httpobs.website.decorators import add_response_headers, sanitized_api_response
-
-__all__ = ['add_response_headers',
-           'sanitized_api_response']
